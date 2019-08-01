1acd97cf3fb22220196d6c68cfd45a192d597d0e
Leo20000603 <yaoleo34@gmail.com>
2019-08-01 14:37:42 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,65 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const auth = require("./auth.json");
+var request = require('request');
+var cheerio = require('cheerio');
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+console.log(Date.now() + " Ping Received");
+response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
 
 client.on("ready", () => {
     console.log("Logged in as Sugar!");
+    var dayMillseconds = 1000*60;
+        setInterval(function(){ // repeat this every 24 hours
+            sendMessage();
+        }, dayMillseconds);
 });
 
-// Create an event listener for new guild members
+function leftToEight(){
+    var d = new Date();
+    return (-d + d.setHours(8,0,0,0));
+}
+
+function sendMessage(){
+    request('https://www.celestialdn.net/', function(err, resp, body) {
+        if (err)
+                throw err;
+        $ = cheerio.load(body);
+        $('input').each(function() { 
+  var url = ($(this).attr('value'));
+  if(String(url).indexOf('DAILY-') != -1){
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    if(today.getHours() == 7 && today.getMinutes() == 5){
+        today = mm + '/' + dd;
+        client.channels.find('name', 'daily-coupon').send(today);
+        client.channels.find('name', 'daily-coupon').send(String(url));
+    }
+  }
+});
+        //console.log(body);
+});
+
+}
 client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
   const channel = member.guild.channels.find(ch => ch.name === 'runway');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
-  channel.send("${member}", {
-    file: "https://cdn.discordapp.com/attachments/596128802594095133/600876942576582688/sugar_guild_welcome.jpg" // Or replace with FileOptions object
+  channel.send(String(member), { file: "https://cdn.discordapp.com/attachments/594358849830387732/601281587560316928/sugarguilddiscord.jpg" // Or replace with FileOptions object 
+  });
 });
-});
-
 client.on('message', msg => {
   if (msg.content === '!ping') {  //testing code with !ping
     msg.channel.send('pong');
